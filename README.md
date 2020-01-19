01a3d2f72508ab35c768087edcfa44bf0fb4b5f8
pmtavares <pedrotavares41@gmail.com>
2020-01-19 18:03:50 +0000

    Added Encryption

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Simples react native with express api and jwt
 
 >> installed mongoose, express and jwt
+
+### Install bcrypt
+
+>> npm install node-gyp
+
+>> npm install bcryptjs --save
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -26,5 +26,30 @@ router.post('/signup', async (req, res)=> {
 
 });
 
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password)
+    {
+        return res.status(422).send({error: 'Must provide an email and password'});
+    }
+
+    const user = await User.findOne({email: email});
+    if(!user){
+        return res.status(404).send({error: 'Email not found'})
+    }
+
+    try{
+        await user.comparePassword(password);
+        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY' );
+        res.send({'token':  token});
+    }
+    catch(error)
+    {
+        return res.status(422).send({error: "Invalid password or email"})
+    }
+    
+
+});
+
 
 module.exports = router;
